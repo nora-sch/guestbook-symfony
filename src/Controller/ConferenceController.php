6015f716215b150faa8a0340967e771ddb234c49
nora-sch <nora.schumann.development@gmail.com>
2023-11-16 22:26:05 +0100

    add eventlistener and subscriber to twig files with conforences info

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -42,12 +42,13 @@ class ConferenceController extends AbstractController
     #[Route('/conference/{id}', name: 'conference')]
     // public function show(Environment $twig, Conference $conference, CommentRepository $commentRepository)
     // public function show(Request $request, Environment $twig, Conference $conference, CommentRepository $commentRepository): Response
-    public function show(Request $request, Conference $conference, CommentRepository $commentRepository): Response
+    public function show(Request $request, Conference $conference, CommentRepository $commentRepository, ConferenceRepository $conferenceRepository): Response
     {
         $offset = max(0, $request->query->getInt('offset', 0));
         $paginator = $commentRepository->getCommentPaginator($conference, $offset);
         // return new Response($twig->render('conference/show.html.twig', [
         return $this->render('conference/show.html.twig', [
+            'conferences' => $conferenceRepository->findAll(),
             'conference' => $conference,
             // 'comments' => $commentRepository->findBy(['conference' => $conference], ['createdAt' => 'DESC']),
             'comments' => $paginator,
