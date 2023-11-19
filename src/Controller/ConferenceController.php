cd018cb561b66b56c1bcbb14ab72865717f5beb3
nora-sch <nora.schumann.development@gmail.com>
2023-11-19 18:47:28 +0100

    add cache --> TODO see one more time / add webpack encore and style the twig with bootstrap

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -48,7 +48,17 @@ class ConferenceController extends AbstractController
         return $this->render('conference/index.html.twig', [
             'conferences' => $conferenceRepository->findAll(),
             // ]));
-        ]);
+            // ]);
+        ])->setSharedMaxAge(3600);
+    }
+
+    #[Route('/conference_header', name: 'conference_header')]
+    public function conferenceHeader(ConferenceRepository $conferenceRepository): Response
+    {
+        return $this->render('conference/header.html.twig', [
+            'conferences' => $conferenceRepository->findAll(),
+        // ]);
+        ])->setSharedMaxAge(3600);
     }
 
     #[Route('/conference/{slug}', name: 'conference')]
