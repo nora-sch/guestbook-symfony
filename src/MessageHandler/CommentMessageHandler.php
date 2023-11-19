719ea94d27b4e037a89c4c41f413fc49f036ab79
nora-sch <nora.schumann.development@gmail.com>
2023-11-19 19:01:29 +0100

    resize images with imagine in optimize transition of workflow and add the step in workflow of CommentMessageHandler

--- src/MessageHandler/CommentMessageHandler.php
+++ src/MessageHandler/CommentMessageHandler.php
@@ -2,6 +2,7 @@
 
 namespace App\MessageHandler;
 
+use App\ImageOptimizer;
 use App\Message\CommentMessage;
 use App\Repository\CommentRepository;
 use App\SpamChecker;
@@ -25,6 +26,8 @@ class CommentMessageHandler
         private WorkflowInterface $commentStateMachine,
         private MailerInterface $mailer,
         #[Autowire('%admin_email%')] private string $adminEmail,
+        private ImageOptimizer $imageOptimizer,
+        #[Autowire('%photo_dir%')] private string $photoDir,
         private ?LoggerInterface $logger = null,
     ) {
     }
@@ -51,7 +54,7 @@ class CommentMessageHandler
             };
             // $this->commentStateMachine->apply($comment, $transition);
             // $this->entityManager->flush();
-            
+
             $this->mailer->send((new NotificationEmail())
                     ->subject('New comment posted')
                     ->htmlTemplate('emails/comment_notification.html.twig')
@@ -59,8 +62,13 @@ class CommentMessageHandler
                     ->to($this->adminEmail)
                     ->context(['comment' => $comment])
             );
-
             $this->bus->dispatch($message);
+        } elseif ($this->commentStateMachine->can($comment, 'optimize')) {
+            if ($comment->getPhotoFilename()) {
+                $this->imageOptimizer->resize($this->photoDir . '/' . $comment->getPhotoFilename());
+            }
+            $this->commentStateMachine->apply($comment, 'optimize');
+            $this->entityManager->flush();
         } elseif ($this->logger) {
             $this->logger->debug('Dropping comment message', ['comment' => $comment->getId(), 'state' => $comment->getState()]);
         }
